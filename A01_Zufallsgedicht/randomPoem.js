4c47d7011fecba5fbc1a4d849b837ae58c319964
waddicor <timothy.john.waddicor@hs-furtwangen.de>
2021-10-21 11:17:44 +0200

    Aufgabe 1 Version 2

--- A01_Zufallsgedicht/randomPoem.js
+++ A01_Zufallsgedicht/randomPoem.js
@@ -1,24 +1,23 @@
 "use strict";
 var randomPoem;
 (function (randomPoem) {
-    let name = ["Peter", "Marco", "Leander", "Herbert", "Gustav", "Donald"];
-    let whattodo = ["gönnt sich", "vernascht", "besiegt", "isst", "sieht", "verachtet"];
-    let thing = ["Grillkäse", "einen yuppie", "Marco", "Leander", "1kg Steak", "Tobis Proteinpulver"];
+    let names = ["Peter", "Marco", "Leander", "Herbert", "Gustav", "Donald"];
+    let whattodos = ["gönnt sich", "vernascht", "besiegt", "isst", "sieht", "verachtet"];
+    let things = ["Grillkäse", "einen yuppie", "Marco", "Leander", "1kg Steak", "Tobis Proteinpulver"];
     let satz = "";
-    for (let i = 6; i <= thing.length; i--) {
+    for (let i = 6; i <= things.length; i--) {
         if (i == 0) {
             break;
         }
-        //console.log(i);
-        getVerse(name, whattodo, thing, satz);
+        getVerse(names, whattodos, things, satz);
     }
-    function getVerse(_name, _whattodo, _thing, _satz) {
-        let zufallname = Math.floor(Math.random() * _name.length);
-        _satz += _name.splice(zufallname, 1) + " ";
-        let zufalltodo = Math.floor(Math.random() * _name.length);
-        _satz += _whattodo.splice(zufalltodo, 1) + " ";
-        let zufallthing = Math.floor(Math.random() * _name.length);
-        _satz += _thing.splice(zufallthing, 1) + " ";
+    function getVerse(_names, _whattodos, _things, _satz) {
+        let zufallname = Math.floor(Math.random() * _names.length);
+        _satz += _names.splice(zufallname, 1) + " ";
+        let zufalltodo = Math.floor(Math.random() * _whattodos.length);
+        _satz += _whattodos.splice(zufalltodo, 1) + " ";
+        let zufallthing = Math.floor(Math.random() * _things.length);
+        _satz += _things.splice(zufallthing, 1) + " ";
         console.log(_satz);
         return _satz;
     }
